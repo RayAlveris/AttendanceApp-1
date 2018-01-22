b636b21bd1e3259f8c7058b4ad1ee956eef531b2
Ray3 <ray3@github.com>
2018-01-22 10:29:00 -0600

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHi, AttendanceApp!\n");
+
+        //Customized welcome message
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter you name: ");
+        String name  = sc.nextLine();
+        System.out.println("Welcome, " + name + "!");
+
+        // Create and output List of absences
+        ArrayList<Integer> absences = new ArrayList<>();
+        Random rand = new Random();
+        for (int i = 0; i < name.length() ; i++) {
+            absences.add(rand.nextInt(11));
+
+
+        }
+        System.out.println("The elements are: " + absences);
     }
 }
