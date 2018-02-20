db7d2ebbd1be63f835d310c5cc2a361d3769ee93
Ray3 <ray3@github.com>
2018-02-20 11:53:56 -0600

    ArrayList of 5 distinct names

--- src/Main.java
+++ src/Main.java
@@ -115,6 +115,7 @@ public class Main {
         studentsNames.add("Semaj");
         studentsNames.add("Darcy");
         studentsNames.add("Marsha");
+        
         return studentsNames;
     }
 
